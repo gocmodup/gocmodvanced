39d4d2c3c32d8cc8fb0902499eb514f13383d1a7
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-04-28 04:50:41 +0700

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -7,5 +7,5 @@ for word in $(cat exclude-patches.txt) ; do
     EXCLUDE_PATCHES+=("-e $word")
 done
 ytversion=$(jq -r '.[] | select(.name == "hide-general-ads") | .compatiblePackages[] | select(.name == "com.google.android.youtube") | .versions[-1]' patches.json)
-chmod +x apkeep && ./apkeep -a com.google.android.youtube@$ytversion .
-java -jar revanced-cli*.jar -m revanced-integrations*.apk -b revanced-patches*.jar ${EXCLUDE_PATCHES[@]} -a com.google.android.youtube*.apk --keystore=ks.keystore -o yt-v$ytversion.apk
+chmod +x apkeep && ./apkeep -a com.facebook.orca .
+java -jar revanced-cli*.jar -m revanced-integrations*.apk -b revanced-patches*.jar ${EXCLUDE_PATCHES[@]} -a com.facebook.orca.apk --keystore=ks.keystore -o yt-v$ytversion.apk
