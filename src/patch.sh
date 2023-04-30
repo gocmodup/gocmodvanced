6ffdcf084837755b18a2b8e0a2b50d6e633031d4
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-04-30 21:24:36 +0700

    Update patch.sh

--- src/patch.sh
+++ src/patch.sh
@@ -18,5 +18,4 @@ done
 patch () {
 chmod +x apkeep && ./apkeep -a $1 .
 java -jar revanced-cli*.jar -m revanced-integrations*.apk -b revanced-patches*.jar -a $1.apk ${EXCLUDE_PATCHES[@]} ${INCLUDE_PATCHES[@]} --keystore=ks.keystore -o ./build/$2
-java -jar revanced-cli*.jar -m revanced-integrations*.apk -b revanced-patches*.jar -a $1.apk ${EXCLUDE_PATCHES[@]} ${INCLUDE_PATCHES[@]} --keystore=ks.keystore -o ./build/$2
 }
