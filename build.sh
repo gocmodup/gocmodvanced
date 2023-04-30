eff5a370825f7a5e9c7273547e6ec8ed5ebb739d
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-05-01 05:21:13 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 source ./src/patch.sh
 get_patch "revanced"
 # Patch Messenger
-get_key_patch "messenger"
-patch "com.facebook.orca" "messenger-revanced"
+#get_key_patch "messenger"
+#patch "com.facebook.orca" "messenger-revanced"
+# Patch Tiktok
+get_ver "block-video-ads"
+patch "tv.twitch.android.app@version" "twitch-rv"
