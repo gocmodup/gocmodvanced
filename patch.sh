af3c64ad84a6f0ccf63d23a7c0e4fd4391660b96
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-04-30 21:14:25 +0700

    update patch.sh

--- patch.sh
+++ patch.sh
@@ -20,6 +20,3 @@ chmod +x apkeep && ./apkeep -a $1 .
 java -jar revanced-cli*.jar -m revanced-integrations*.apk -b revanced-patches*.jar -a $1.apk ${EXCLUDE_PATCHES[@]} ${INCLUDE_PATCHES[@]} --keystore=ks.keystore -o ./build/$2
 java -jar revanced-cli*.jar -m revanced-integrations*.apk -b revanced-patches*.jar -a $1.apk ${EXCLUDE_PATCHES[@]} ${INCLUDE_PATCHES[@]} --keystore=ks.keystore -o ./build/$2
 }
-# get_patch "revanced"
-get_key_patch "messenger"
-#patch "com.facebook.orca" "messenger-revanced.apk"
