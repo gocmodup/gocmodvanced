5a22e5988a2184f7c7cd62dbcb4a856a7f5a4f04
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-04-30 19:50:37 +0700

    update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+get_patch() {
 for repos in revanced-patches revanced-cli revanced-integrations; do
     curl -s "https://api.github.com/repos/revanced/$repos/releases/latest" | jq -r '.assets[].browser_download_url' | xargs -n 1 curl -sL -O
 done
+}
+get_key_patch() {
+EXCLUDE_PATCHES=()
+for word in $(cat exclude-patches.txt) ; do
+    EXCLUDE_PATCHES+=("-e $word")
+done
+INCLUDE_PATCHES=()
+for word in $(cat include-patches.txt) ; do
+    INCLUDE_PATCHES+=("-i $word")
+done
+}
+patch() {
 chmod +x apkeep && ./apkeep -a com.facebook.orca .
+}
 java -jar revanced-cli*.jar -m revanced-integrations*.apk -b revanced-patches*.jar -a com.facebook.orca.apk --keystore=ks.keystore -o messenger-revanced.apk
