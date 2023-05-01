c719547acf0980ffb875b9acbb6983b681ea5f60
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-05-01 23:35:11 +0700

    update build-rve.sh

--- build-rve.sh
+++ build-rve.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 # Revanced Extended build
 source tools.sh
+release=$(curl -s "https://api.github.com/repos/inotia00/revanced-patches/releases/latest")
+asset=$(echo "$release" | jq -r '.assets[] | select(.name | test("revanced-patches.*\\.jar$")) | .browser_download_url')
+curl -sL -O "$asset"
+ls revanced-patches*.jar >> new.txt
+rm -f revanced-patches*.jar
+release=$(curl -s "https://api.github.com/repos/luxysiv/revanced-build/releases/latest")
+asset=$(echo "$release" | jq -r '.assets[] | select(.name == "revanced-extended-version.txt") | .browser_download_url')
+curl -sL -O "$asset"
+if diff -q revanced-extended-version.txt new.txt >/dev/null ; then
+echo "Old patch!!! Not build"
+exit 1
+else
+rm -f *.txt
 dl_gh "inotia00"
 # Patch YouTube Extended
-
 get_patches_key "youtube-revanced-extended"
 get_ytrve_ver
 dl_yt
@@ -13,3 +25,4 @@ get_patches_key "youtube-music-revanced-extended"
 dl_ytms
 patch "youtube-music" "youtube-music-revanced-extended"
 ls revanced-patches*.jar >> revanced-extended-version.txt
+fi
