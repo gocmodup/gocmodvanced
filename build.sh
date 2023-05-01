88437233c69244af66fd478455dbefdafa1136d4
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-05-01 15:54:51 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+# Revanced build
 source tools.sh
-get_patch "revanced"
-# Patch Messenger
-#get_key_patch "messenger"
-#patch "com.facebook.orca" "messenger-revanced"
+dl_gh "revanced" 
+# Messenger
+get_patches_key "messenger"
+dl_mes
+patch "messenger" "messenger-revanced"
 # Patch Twitch
-#get_key_patch "twitch"
-#get_tw_ver
-#patch "tv.twitch.android.app@$twversion" "twitch-revanced-v$twversion"
+get_key_patch "twitch"
+get_tw_ver
+patch "twitch" "twitch-revanced-v$twversion"
 # Patch Tiktok 
 get_key_patch "tiktok"
 get_tt_ver
-patch "com.ss.android.ugc.trill@$ttversion -d huawei-app-gallery" "tiktok-revanced-v$ttversion"
+patch "tiktok" "tiktok-revanced-v$ttversion"
