ac8529c2dacfdd2074a019ea9244b60f9b1bce81
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-05-01 16:04:01 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -9,8 +9,10 @@ patch "messenger" "messenger-revanced"
 # Patch Twitch
 get_patches_key "twitch"
 get_tw_ver
+dl_twitch
 patch "twitch" "twitch-revanced-v$twversion"
 # Patch Tiktok 
 get_patches_key "tiktok"
 get_tt_ver
+dl_tt
 patch "tiktok" "tiktok-revanced-v$ttversion"
