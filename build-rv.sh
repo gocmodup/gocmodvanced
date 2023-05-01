2236aaa8e657a2769b410d9172f6c0b075f9c9fe
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-05-01 23:33:17 +0700

    Update build-rv.sh

--- build-rv.sh
+++ build-rv.sh
@@ -10,10 +10,10 @@ release=$(curl -s "https://api.github.com/repos/luxysiv/revanced-build/releases/
 asset=$(echo "$release" | jq -r '.assets[] | select(.name == "revanced-version.txt") | .browser_download_url')
 curl -sL -O "$asset"
 if diff -q patch-version.txt new.txt >/dev/null ; then
-    echo "Old patch!!! Not build"
-    exit 1
+echo "Old patch!!! Not build"
+exit 1
 else
-    rm -f *.txt
+rm -f *.txt
 dl_gh "revanced" 
 # Messenger
 get_patches_key "messenger"
